c9733e1b352ad14f717925205825d9671a15981a
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-10 23:42:22 +0530

    Added the code

---
+++ main.py
@@ -0,0 +1,19 @@
+# The below function calculates if a number is odd or even
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
+# Print a Welcome message
+print("Welcome to Odd or Even!")
+
+# Get the number as an input
+number = int(input("Which number do you want to check? "))
+
+# Check for the conditions and print the final answer
+if number % 2 == 1:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
